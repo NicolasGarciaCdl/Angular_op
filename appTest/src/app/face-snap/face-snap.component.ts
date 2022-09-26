b694ea2609c89c509ffc369a156d3cfa2d2b4fda
Nicolas Garcia <garky33@hotmail.com>
2022-09-26 20:57:51 +0200

    add scss face-snap-card and font on index head

--- appTest/src/app/face-snap/face-snap.component.ts
+++ appTest/src/app/face-snap/face-snap.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {}
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  imgUrl!: string;
+
+  ngOnInit() {
+    this.title = 'toto';
+    this.description = 'une jolie description';
+    this.createdAt = new Date();
+    this.snaps = 6;
+    this.imgUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+  }
+
+}
