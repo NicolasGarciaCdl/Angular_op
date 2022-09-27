6fdbddee711cb7c235756c67341e77c370afe2f4
Nicolas Garcia <garky33@hotmail.com>
2022-09-27 19:29:27 +0200

    add models directory and face-snap.model.ts

---
+++ appTest/src/app/models/face-snap.model.ts
@@ -0,0 +1,9 @@
+export class FaceSnapModel {
+
+  constructor( public title:string,
+               public description: string,
+               public imgUrl: string,
+               public createdAt: Date,
+               public snaps:number) {
+  }
+}
