67cfe62a8191c5bad0ae6d1d1e5db745cce9c452
Nicolas Garcia <garky33@hotmail.com>
2022-09-27 19:12:22 +0200

    add onToggleLike function on click

--- appTest/src/app/face-snap/face-snap.component.ts
+++ appTest/src/app/face-snap/face-snap.component.ts
@@ -11,13 +11,24 @@ export class FaceSnapComponent implements OnInit {
   createdAt!: Date;
   snaps!: number;
   imgUrl!: string;
+  buttonText!:string;
 
   ngOnInit() {
     this.title = 'toto';
     this.description = 'une jolie description';
     this.createdAt = new Date();
     this.snaps = 6;
-    this.imgUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+    this.imgUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.buttonText = 'Like!'
   }
 
+  onToggleLike(){
+    if(this.buttonText == 'Like!'){
+      this.snaps++;
+      this.buttonText = 'Unlike!';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Like!';
+    }
+  }
 }
